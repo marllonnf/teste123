777858812df7df60bb18189c46ddcb29e1bf76e6
marllonnf <marllonnarcizo2017@gmail.com>
2017-04-14 14:14:28 -0300

    1
    1

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ $mail->Debugoutput = 'html';
 //Set the hostname of the mail server
 $mail->Host = 'smtp.gmail.com';
 // use
-// $mail->Host = gethostbyname('smtp.gmail.com');
+// $mail->Host = gethostbyname('https://marllonnf.github.io/teste123/.');
 // if your network does not support SMTP over IPv6
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 587;
@@ -33,11 +33,11 @@ $mail->Username = "username@gmail.com";
 //Password to use for SMTP authentication
 $mail->Password = "yourpassword";
 //Set who the message is to be sent from
-$mail->setFrom('from@example.com', 'First Last');
+$mail->setFrom('marllonnarcizo2017@gmail.com', 'First Last');
 //Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
+$mail->addReplyTo('marllonnarcizo2017@gmail.com', 'First Last');
 //Set who the message is to be sent to
-$mail->addAddress('whoto@example.com', 'John Doe');
+$mail->addAddress('marllonnarcizo2017@gmail.com', 'John Doe');
 //Set the subject line
 $mail->Subject = 'PHPMailer GMail SMTP test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
